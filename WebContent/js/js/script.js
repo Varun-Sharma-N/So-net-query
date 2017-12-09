a4dedb6925c2e64c570c7d2f23191eb3cca2a665
Varun Sharma N <varun15sharma15@gmail.com>
2017-12-09 12:03:19 -0800

    Add files via upload

---
+++ WebContent/js/js/script.js
@@ -0,0 +1,20 @@
+// <![CDATA[
+$(function() {
+
+  // Slider
+  $('#coin-slider').coinslider({width:960,height:360,opacity:1});
+
+  // Radius Box
+  //$('.content .mainbar img.fl, .content .sidebar .gadget, .fbg_resize').css({"border-radius":"8px", "-moz-border-radius":"8px", "-webkit-border-radius":"8px"});
+  //$('.fbg_resize2').css({"border-radius":"9px", "-moz-border-radius":"9px", "-webkit-border-radius":"9px"});
+  //$('.content p.pages span, .content p.pages a').css({"border-radius":"16px", "-moz-border-radius":"16px", "-webkit-border-radius":"16px"});
+  //$('.menu_nav').css({"border-bottom-left-radius":"16px", "border-bottom-right-radius":"16px", "-moz-border-radius-bottomleft":"16px", "-moz-border-radius-bottomright":"16px", "-webkit-border-bottom-left-radius":"16px", "-webkit-border-bottom-right-radius":"16px"});
+
+});	
+
+// Cufon
+Cufon.replace('h1, h2, h3, h4, h5, h6', { hover: true });
+//Cufon.replace('h1', { color: '-linear-gradient(#fff, #ffaf02)'});
+//Cufon.replace('h1 small', { color: '#8a98a5'});
+
+// ]]>
