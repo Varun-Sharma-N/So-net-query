0d159da3f60fce8e249a7a071db792395f5f6b52
Varun Sharma N <varun15sharma15@gmail.com>
2017-12-09 11:53:57 -0800

    Add files via upload

--- src/com/social/graph.java
+++ src/com/social/graph.java
@@ -1,1 +1,23 @@
+package com.social;
 
+
+
+public class graph {
+
+	private String hospitaldata;
+	private int count;
+	public String getso_net() {
+		return hospitaldata;
+	}
+	public void setso_net(String hospitaldata) {
+		this.hospitaldata = hospitaldata;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	
+}
