9cc75b25888fa22961abb0f82ee95072adb683f6
Varun Sharma N <varun15sharma15@gmail.com>
2017-12-09 11:44:23 -0800

    Create graph.java

---
+++ com/social/graph.java
@@ -0,0 +1,23 @@
+package com.social;
+
+
+
+public class graph {
+
+	private String hospitaldata;
+	private int count;
+	public String gethospitalname() {
+		return hospitaldata;
+	}
+	public void sethospitalname(String hospitaldata) {
+		this.hospitaldata = hospitaldata;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	
+}
